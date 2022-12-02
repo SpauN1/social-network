0277bfc99670637aafef02669227da32b24d05b4
Spaun1 <andreispaun7@gmail.com>
2022-12-02 19:49:13 +0300

    add styles for active link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,31 +3,33 @@ import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const setActive = ({ isActive }) => (isActive ? styles.active : styles.item);
+
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
-          <NavLink className={styles.item} to="/profile">
+        <li className={styles.item}>
+          <NavLink className={setActive} to="/profile">
             Profile
           </NavLink>
         </li>
-        <li>
-          <NavLink className={styles.item} to="/dialogs">
+        <li className={styles.item}>
+          <NavLink className={setActive} to="/dialogs">
             Messages
           </NavLink>
         </li>
-        <li>
-          <NavLink className={styles.item} to="/News">
+        <li className={styles.item}>
+          <NavLink className={setActive} to="/News">
             News
           </NavLink>
         </li>
-        <li>
-          <NavLink className={styles.item} to="/Music">
+        <li className={styles.item}>
+          <NavLink className={setActive} to="/Music">
             Music
           </NavLink>
         </li>
-        <li>
-          <NavLink className={styles.item} to="/Settings">
+        <li className={styles.item}>
+          <NavLink className={setActive} to="/Settings">
             Settings
           </NavLink>
         </li>
