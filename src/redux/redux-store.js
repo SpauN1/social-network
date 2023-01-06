136ebd8e310d3f7718279d8cf2b77909794d827a
Spaun1 <andreispaun7@gmail.com>
2023-01-06 14:33:47 +0300

    feat: add container components for usersContainer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,11 +3,13 @@ import { createStore, combineReducers } from 'redux';
 import profileReducer from './reducers/profileReducer';
 import dialogsReducer from './reducers/dialogsReducer';
 import sidebarReducer from './reducers/sidebarReducer';
+import usersReducer from "./reducers/usersReducer";
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
+  usersPage: usersReducer,
 });
 
 const store = createStore(reducers);
