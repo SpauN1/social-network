499a5c8afd8f4c4c2c0ea1264ecf52bb95fccf9c
Spaun1 <andreispaun7@gmail.com>
2023-01-15 02:34:58 +0300

    fix: new paths to url profile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from "./components/Profile/ProfileContainer";
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 import './App.css';
 
@@ -16,7 +16,9 @@ function App() {
         <Navbar />
         <div className="content-wrapper">
           <Routes>
-            <Route path="/profile/:userId?" element={<ProfileContainer />} />
+            <Route path="profile" element={<ProfileContainer />}>
+              <Route path=":userId" element={<ProfileContainer />} />
+            </Route>
             <Route path="/dialogs/*" element={<DialogsContainer />} />
             <Route path="/users" element={<UsersContainer />} />
           </Routes>
