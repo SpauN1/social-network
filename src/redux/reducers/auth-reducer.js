a6edbf0fb6a3bca4c734200bdfb8b3a85a8719ac
Spaun1 <andreispaun7@gmail.com>
2023-01-16 22:46:41 +0300

    feat: add auth-reducer

---
+++ src/redux/reducers/auth-reducer.js
@@ -0,0 +1,26 @@
+const SET_USER_DATA = 'SET_USER_DATA';
+
+const initialState = {
+  userId: null,
+  email: null,
+  login: null,
+  isFetching: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.data,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const setUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: {userId, email, login} });
+
+export default authReducer;
+export { setUserData };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,13 +3,15 @@ import { createStore, combineReducers } from 'redux';
 import profileReducer from './reducers/profileReducer';
 import dialogsReducer from './reducers/dialogsReducer';
 import sidebarReducer from './reducers/sidebarReducer';
-import usersReducer from "./reducers/usersReducer";
+import usersReducer from './reducers/usersReducer';
+import authReducer from './reducers/auth-reducer';
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
 
 const store = createStore(reducers);
