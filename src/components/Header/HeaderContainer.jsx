b734bbf5e3ebb092619a89be38bf63afed934a7d
Spaun1 <andreispaun7@gmail.com>
2023-01-27 20:54:56 +0300

    refactor: transfer requests to api

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,22 +1,18 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 import Header from './Header';
 import { setAuthUserData } from '../../redux/reducers/auth-reducer';
+import { authAPI } from '../../api/api';
 
 class HeaderContainer extends Component {
   componentDidMount() {
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        if (response.data.resultCode === 0) {
-          const { id, login, email } = response.data.data;
-          this.props.setAuthUserData(id, email, login);
-        }
-      });
+    authAPI.me().then((response) => {
+      if (response.data.resultCode === 0) {
+        const { id, login, email } = response.data.data;
+        this.props.setAuthUserData(id, email, login);
+      }
+    });
   }
 
   render() {
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,10 +1,10 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
 
 import Profile from './Profile';
 import { setUserProfile } from '../../redux/reducers/profileReducer';
+import { usersAPI } from '../../api/api';
 
 function withRouter(Component) {
   function ComponentWithRouterProp(props) {
@@ -25,11 +25,9 @@ class ProfileContainer extends Component {
       userId = 2;
     }
 
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0//profile/` + userId)
-      .then((response) => {
-        this.props.setUserProfile(response.data);
-      });
+    usersAPI.getProfile(userId).then((response) => {
+      this.props.setUserProfile(response.data);
+    });
   }
   render() {
     return <Profile {...this.props} profile={this.props.profile} />;
