4dbad4edaefbbddcdfcc7de19ab814545490fbfb
Spaun1 <andreispaun7@gmail.com>
2023-02-04 22:06:14 +0300

    refactor: status update sync

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -25,12 +25,20 @@ class ProfileStatus extends Component {
     });
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onDoubleClick={this.activateEditMode || "---"}>
+            <span onDoubleClick={this.activateEditMode || '---'}>
               {this.props.status}
             </span>
           </div>
